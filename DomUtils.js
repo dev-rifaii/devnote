56d0b434df4a319535541663aa6f5ca84da8f711
Mo <moe.rifaii77@gmail.com>
2024-01-10 23:30:20 +0200

    Add code highlighting

--- DomUtils.js
+++ DomUtils.js
@@ -1,9 +1,9 @@
 export function setTextForElementById(elementId, text) {
-    document.getElementById(elementId).textContent = text;
+    document.getElementById(elementId).innerHTML = text;
 }
 
 export function createElementWithText(elementName, text) {
     const newElement = document.createElement(elementName);
-    newElement.textContent = text;
+    newElement.innerHTML = text;
     return newElement;
 }
