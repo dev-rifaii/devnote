4b3ccade8364b330884b18b08939eabe5f9870ea
Mo <moe.rifaii77@gmail.com>
2024-01-14 23:50:59 +0200

    Add header and footer

--- CustomElementsBuilder.js
+++ CustomElementsBuilder.js
@@ -1,5 +1,10 @@
 import * as domUtils from "./DomUtils.js";
 
+export function renderCommonElements() {
+    renderHeader();
+    renderFooter();
+}
+
 export function createCodeSection(code) {
     const codeSectionContainer = domUtils.emptyDiv();
     codeSectionContainer.classList.add("code-section");
@@ -12,8 +17,35 @@ export function createCodeSection(code) {
 function buildCodeSectionHeader() {
     const copyCodeButton = domUtils.createElementWithText("button", "copy");
 
+    copyCodeButton.addEventListener("click", event => {
+        const codeToCopy = copyCodeButton.parentElement.parentElement.lastElementChild.textContent;
+        navigator.clipboard.writeText(codeToCopy);
+        copyCodeButton.textContent = "copied to clipboard";
+        setTimeout(() => { copyCodeButton.textContent = "Copy"; }, 1000);
+    })
+
     const codeSectionHeaderContainer = domUtils.emptyDiv();
     codeSectionHeaderContainer.classList.add("code-section-header");
     codeSectionHeaderContainer.appendChild(copyCodeButton);
     return codeSectionHeaderContainer;
+}
+
+function renderHeader() {
+    const dashboardButton = domUtils.createElementWithText("button", "Dashboard");
+    const createButton = domUtils.createElementWithText("button", "Create");
+    dashboardButton.classList.add("navbar-button");
+    createButton.classList.add("navbar-button");
+
+    const header = document.querySelector("header");
+    header.appendChild(dashboardButton);
+    header.appendChild(createButton);
+}
+
+function renderFooter() {
+    const githubLink = domUtils.createElementWithText("a", "GitHub");
+    githubLink.href = "https://github.com/dev-rifaii/";
+    githubLink.classList.add("footer-element");
+
+    const footer = document.querySelector("footer");
+    footer.appendChild(githubLink);
 }
